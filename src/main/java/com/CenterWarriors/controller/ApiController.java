43f59b3bf86e31482c8e27f3f3f2aa10dd190de8
Melany <mefrancom@unal.edu.co>
2025-02-06 13:25:39 -0500

    correcion errores

---
+++ src/main/java/com/CenterWarriors/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.CenterWarriors.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class ApiController {
+
+    @GetMapping
+    public String welcome() {
+        return "Bienvenido a la API de Center Warriors";
+    }
+}
